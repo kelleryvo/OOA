bf2e3b754bc8732d274360ede749b0b16acdb327
Dennis Schappi <dennisschaeppi@outlook.com>
2017-05-15 14:23:58 +0200

    Party und Gastgeber

---
+++ src/Gastgeber.java
@@ -0,0 +1,14 @@
+/**
+ * Created by denni on 15.05.2017.
+ */
+public class Gastgeber extends Person {
+    Party party = new Party();
+
+    public void createParty(){
+
+    }
+
+    public void getParty(){
+
+    }
+}
--- src/Person.java
+++ src/Person.java
@@ -2,5 +2,16 @@
  * Created by yvokeller on 15.05.17.
  */
 public class Person {
+    private String vorname;
+    private String nachname;
 
+    public String getVorname(){
+        this.vorname = vorname;
+        return vorname;
+    }
+
+    public String getNachname(){
+        this.nachname = nachname;
+        return nachname;
+    }
 }
