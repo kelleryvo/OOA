9fe8a4b419ca9fc1b927c361ed5cbed73824cb51
BuildTools <unconfigured@null.spigotmc.org>
2017-05-15 14:23:46 +0200

    tes

---
+++ src/Party.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by yvokeller on 15.05.17.
+ */
+public class Party {
+    private Date date;
+    private String ort;
+
+    private Gastgeber gastgeber;
+    private ArrayList<Invitation> invitations = new ArrayList<>();
+
+}
